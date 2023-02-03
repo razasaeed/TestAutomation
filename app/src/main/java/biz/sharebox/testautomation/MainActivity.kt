5f463c79a2af1a9495f82edf2cb3f8ac7195d42c
Raza Saeed <raza.saeed@devgate.ca>
2023-02-04 04:05:18 +0500

    Initial commit

---
+++ app/src/main/java/biz/sharebox/testautomation/MainActivity.kt
@@ -0,0 +1,11 @@
+package biz.sharebox.testautomation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
