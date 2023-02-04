3617df1eef35c8459c379fb3e1cc804b696861f7
Raza Saeed <raza.saeed@devgate.ca>
2023-02-04 16:47:33 +0500

    Test hooks 3

--- app/src/main/java/biz/sharebox/testautomation/MainActivity.kt
+++ app/src/main/java/biz/sharebox/testautomation/MainActivity.kt
@@ -1,13 +1,15 @@
 package biz.sharebox.testautomation
 
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("testing", "hook test")
 
     }
 }
